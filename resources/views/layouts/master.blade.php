06ccde8df3c17e92fcbad079cf1e21012a88e815
Muhammad Azeem <emazeem07@gmail.com>
2020-11-22 15:11:45 +0500

    sidebar issue

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -63,7 +63,20 @@
 
     if ($(window).width() < 480){
         $(".sidebar").addClass("toggled");
+
     }
+
+     $(window).resize(function() {
+     if ($(window).width() < 768) {
+     $('.sidebar .collapse').collapse('hide');}
+
+    // // Toggle the side navigation when window is resized below 480px
+     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+     $("body").addClass("sidebar-toggled");
+     $(".sidebar").addClass("toggled");
+     $('.sidebar .collapse').collapse('hide');
+     }
+     });
 
 </script>
 
