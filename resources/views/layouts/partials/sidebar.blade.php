abbd919ae30cc31f5e5fa2ca3c93979c48add6d6
emazeem <emazeem07@gmail.com>
2020-11-22 17:54:15 +0500

    sidebar problem

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -15,9 +15,13 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item {{(Request::url()==url(''))?"active":""}}">
-        <a class="nav-link" href="{{url('/')}}">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+        <a class="nav-link p-1 p-md-2 text-sm-left" href="{{url('/')}}">
+
+            <span class="text-xs border-md-bottom">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                Dashboard
+            </span>
+        </a>
     </li>
     <!-- Heading -->
     <div class="sidebar-heading">
@@ -27,18 +31,24 @@
     @foreach($menus as $menu)
         @can($menu->slug)
         <li class="nav-item {{(Request::url()==url(''.$menu->url))?"active":""}}">
-            <a class="nav-link p-0 p-md-2 text-sm-left" href="{{url($menu->url)}}">
-                <i class="{{$menu->icon}} d-md-inline d-none"></i>
-                <span>{{$menu->name}}</span>
+            <a class="nav-link p-1 p-md-2 text-sm-left" href="{{url($menu->url)}}">
+                <span class="text-xs border-md-bottom">
+                    <i class="{{$menu->icon}} d-inline text-xs"></i>
+                    {{$menu->name}}
+                </span>
             </a>
         </li>
         @endcan
     @endforeach
     @can('settings-index')
         <li class="nav-item {{(Request::url()==url(''.'settings'))?"active":""}}">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                <i class="fas fa-fw fa-cog"></i>
-                <span>Settings</span>
+            <a class="nav-link collapsed  p-1 p-md-2 text-sm-left" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                <span class="text-xs border-md-bottom">
+                <i class="fas fa-fw fa-cog d-inline text-xs"></i>
+
+                Settings
+                </span>
+
             </a>
             <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                 <div class="bg-primary text-white py-2 collapse-inner rounded">
