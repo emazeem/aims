64975f62b84ddd98893b1f55a559bcb5b79217c4
emazeem <emazeem07@gmail.com>
2020-12-03 11:26:14 +0500

    changes

--- resources/views/jobs/invoice.blade.php
+++ resources/views/jobs/invoice.blade.php
@@ -26,7 +26,7 @@
     <div class="col-12 font-style mt-2">
         <div class="row">
             <div class="col-2 text-center">
-                <img src="{{url('/img/aims.png')}}" class="mt-2 ml-2" width="100">
+                <img src="{{url('/img/AIMS.png')}}" class="mt-2 ml-2" width="100">
             </div>
             <div class="col-10 custom-bottom-border text-right p-0">
                 <p class="font-14 b">INVOICE</p>
