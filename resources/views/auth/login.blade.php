333df1add2c62bd0c625b0f596d8cf9772a87a1a
emazeem <emazeem07@gmail.com>
2020-12-04 18:31:11 +0500

    design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,16 +54,7 @@
                                 <button type="submit" class="btn float-right btn-outline-light">
                                     {{ __('Login') }}
                                 </button>
-
-                                --}}
-{{--@if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif--}}{{--
-
-                            </div>
-                        </div>
+                            </div></div>
                     </form>
                 </div>
             </div>
