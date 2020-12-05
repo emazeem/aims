4f8c91d70149fb2797cf62f7a7925b24db661cf3
emazeem <emazeem07@gmail.com>
2020-12-05 10:55:53 +0500

    changes

--- resources/views/jobs/jobtag.blade.php
+++ resources/views/jobs/jobtag.blade.php
@@ -30,7 +30,7 @@
 <style>
     @media print {
         @page {
-            size: 25.4mm 25.4mm; /* DIN A4 standard, Europe */
+            size: 25.4mm 50.8mm; /* DIN A4 standard, Europe */
             margin: 0;
         }
         html, body {
@@ -74,7 +74,6 @@
         </tr>
         </tbody>
     </table>
-
 </div>
 </body>
 </html>
