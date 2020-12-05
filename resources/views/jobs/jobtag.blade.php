451ea628788ddcbac29bfcd1b1c74e90cbd957c2
emazeem <emazeem07@gmail.com>
2020-12-05 12:01:31 +0500

    changes

--- resources/views/jobs/jobtag.blade.php
+++ resources/views/jobs/jobtag.blade.php
@@ -27,48 +27,26 @@
 </style>
 --}}
 
-<style>
-    @media print {
-        @page {
-            size: 50.8mm 25.4mm; /* DIN A4 standard, Europe */
-            margin: 0;
-        }
-        html, body {
-            overflow: visible;
-        }
-        body * ,.sticker-size *{
-            visibility: hidden;
-        }
-        .sticker-size, .sticker-size * {
-            visibility: visible;
-        }
-    }
-    .sticker-size {
-        width: 50.8mm;
-        height: 25.4mm;
-        font-size: 10px;
-    }
-</style>
 <div class="container">
-
-
-    <table class="table table-bordered sticker-size">
+    <table class="table table-bordered col-12">
         <tbody>
-        <tr>
-            <th colspan="2" class="text-center"> ITEM TAG</th>
-            <td rowspan="3" class="text-center py-3">
-                {{QrCode::size(30)->generate(route('checkin.create',[$loc,$tag->id]))}}
+        <tr >
+            <th style="padding:20px" colspan="2" class="text-center"> <h1>ITEM TAG</h1></th>
+            <td rowspan="3" class="text-center py-5">
+                <span class="py-5">
+                {{QrCode::size(200)->generate(route('checkin.create',[$loc,$tag->id]))}}
+                </span>
             </td>
         </tr>
         <tr>
-            <th>Job No : {{$tag->job_id}}</th>
+            <th class="p-3"><h1>Job No : {{$tag->job_id}}</h1></th>
         </tr>
         <tr>
-            <th colspan="2">Item # : {{$index}} of {{$total}}</th>
+            <th colspan="2" class="p-3"><h1>Item # : {{$index}} of {{$total}}</h1></th>
         </tr>
         <tr>
             <td class="text-center p-0" colspan="3">
-                <small>Doc # : AIMS-TM-FRM-12 , Rev#: 01</small>
+                <h3 class="p-2">Doc # : AIMS-TM-FRM-12 , Rev#: 01</h3>
             </td>
         </tr>
         </tbody>
