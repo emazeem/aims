e985d7deb7d41c7e8f849b1e587c81a712d06dcf
emazeem <emazeem07@gmail.com>
2020-12-21 17:50:24 +0500

    changes

--- resources/views/invoicingledger/index.blade.php
+++ resources/views/invoicingledger/index.blade.php
@@ -13,7 +13,7 @@
 
             <span>
 
-            <a href="{{route('pra')}}" class="btn btn-secondary btn-sm"><i class="fa fa-print"></i></a>
+                <a href="{{route('pra')}}" class="btn btn-outline-secondary btn-sm"><small><i class="fa fa-print"></i> PRA</small></a>
             {{--<a href="{{route('invoicing_ledger.create')}}" class="btn btn-success btn-sm"><i class="fa fa-plus-circle"></i> Add Entry</a>
             --}}</span>
         </div>
