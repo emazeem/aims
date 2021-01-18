bc6d17d9c2af40d02ddfe430665cb8c1b8d36b05
emazeem <emazeem07@gmail.com>
2021-01-18 19:50:11 +0500

    chanbge

--- resources/views/menus.blade.php
+++ resources/views/menus.blade.php
@@ -343,7 +343,7 @@
                             <label for="parent">Parent</label>
                             <div class="form-check form-check-inline" style="width: 100%">
                                 <select class="form-control" id="edit_parent" name="parent">
-                                    <option>None</option>
+                                    <option value="">None</option>
                                     @foreach($parents as $parent)
                                         <option value="{{$parent->id}}">{{$parent->name}}</option>
                                     @endforeach
