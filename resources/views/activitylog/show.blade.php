f1e925a33f2410802260d3d6580333f1f50af37b
emazeem <emazeem07@gmail.com>
2021-02-02 09:37:48 +0500

    'ch;

--- resources/views/activitylog/show.blade.php
+++ resources/views/activitylog/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('content')
-    <div class="col-12 p-0 m-0">
+    <div class="row">
         <div class="x_panel">
             <div class="x_title">
                 <h2>Activity Log <small></small></h2>
