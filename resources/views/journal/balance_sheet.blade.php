e9e9781c4e5d0f2f2afc0c9ece30ba20dd0318e1
DanaiHeyligen <emazeem07@gmail.com>
2021-05-29 09:15:43 +0500

    ch

--- resources/views/journal/balance_sheet.blade.php
+++ resources/views/journal/balance_sheet.blade.php
@@ -18,7 +18,7 @@
     <div class="col-12 font-style mt-2">
         <div class="row">
             <div class="col-2 text-center custom-border">
-                <img src="{{url('/img/aims.png')}}" class="mt-2 ml-2" width="100">
+                <img src="{{url('/img/AIMS.png')}}" class="mt-2 ml-2" width="100">
             </div>
             <div class="col-7 border-left-right-0 custom-border">
                 <p class="text-center b font-14 mt-4 text-capitalize">
--- resources/views/journal/ledger.blade.php
+++ resources/views/journal/ledger.blade.php
@@ -15,7 +15,7 @@
     <div class="col-12 font-style mt-2">
         <div class="row">
             <div class="col-2 text-center border border-dark border-right-0 p-2">
-                <img src="{{url('/img/aims.png')}}" class="mt-2 ml-2" width="100">
+                <img src="{{url('/img/AIMS.png')}}" class="mt-2 ml-2" width="100">
             </div>
             <div class="col-10 border border-dark  p-2" >
                 <h1 class="text-center b font-14 mt-4 text-capitalize">
