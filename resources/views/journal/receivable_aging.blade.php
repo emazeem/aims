20be7b1a9bdc060d2c04ced732ced9aeea15dd36
DanaiHeyligen <emazeem07@gmail.com>
2021-05-29 09:14:34 +0500

    ch

--- resources/views/journal/receivable_aging.blade.php
+++ resources/views/journal/receivable_aging.blade.php
@@ -13,7 +13,7 @@
     <div class="col-12 font-style mt-2">
         <div class="row">
             <div class="col-2 text-center custom-border">
-                <img src="{{url('/img/aims.png')}}" class="mt-2 ml-2" width="100">
+                <img src="{{url('/img/AIMS.png')}}" class="mt-2 ml-2" width="100">
             </div>
             <div class="col-10 border-left-0 custom-border" >
                 <h1 class="text-center b mt-4 text-capitalize">
