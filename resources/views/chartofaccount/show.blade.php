e773b1af33ab458c420c3808375bc0381c04a04b
DanaiHeyligen <emazeem07@gmail.com>
2021-06-07 17:51:46 +0500

    ch

--- resources/views/chartofaccount/show.blade.php
+++ resources/views/chartofaccount/show.blade.php
@@ -18,63 +18,172 @@
 
     <div class="row ">
         <div class="col-12">
-            <h1 class="border-bottom pull-left"><i class="fa fa-money"></i> Chart of Account</h1>
+            <h3 class=" pull-left"><i class="feather icon-menu"></i> Chart of Account</h3>
             <div class="pull-right">
                 <a class="btn btn-info btn-sm" href="{{route('acc_level_four.print')}}"><i class="fa fa-print"></i> Print COA </a>
             </div>
         </div>
         <div class="col-12">
-            <ul>
+            <ul id="tree">
                 @foreach($accounts as $account)
-                    <li class="level-1">{{$account->title}}</li>
-                        <ul >
-                            @foreach($account->leveltwo as $leveltwo)
-                                <li class="level-2">{{$leveltwo->title}}</li>
-                                    <ul class="nested">
-                                        @foreach($leveltwo->levelthree as $levelthree)
-                                            <li class="level-3">{{$levelthree->title}}</li>
-                                            <ul>
-                                            @foreach($levelthree->levelfour as $chart)
-                                                    <li class="level-4">
-                                                        {{$chart->acc_code}}-{{$chart->title}}
-                                                    </li>
-                                                @endforeach
-                                            </ul>
+                    <li>{{$account->title}}
+                    <ul >
+                        @foreach($account->leveltwo as $leveltwo)
+                            <li>{{$leveltwo->title}}
+                            <ul>
+                                @foreach($leveltwo->levelthree as $levelthree)
+                                    <li>{{$levelthree->title}}
+                                    <ul>
+                                        @foreach($levelthree->levelfour as $chart)
+                                            <li>
+                                                {{$chart->acc_code}}-{{$chart->title}}
+                                            </li>
                                         @endforeach
                                     </ul>
-                            @endforeach
-                        </ul>
+                                    </li>
+                                @endforeach
+                            </ul>
+                            </li>
+                        @endforeach
+                    </ul>
+                    </li>
                 @endforeach
             </ul>
         </div>
+
     </div>
-    <style>
-        .level-1{
-            font-size: 22px;
-            font-weight: bolder;
-            color: rgb(223, 10, 17);
-            list-style-type: circle;
-        }
-        .level-2{
-            font-size: 20px;
-            font-weight: bold;
-            color: black;
 
-            list-style-type: disc;
-        }
-        .level-3{
-            font-size: 20px;
-            font-weight: normal;
-            color: rgb(223, 10, 17);
-            list-style-type: circle;
-        }
-        .level-4{
-            font-size: 18px;
-            font-weight: normal;
-            color: black;
 
-            list-style-type: disc;
-        }
-    </style>
 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+    
+<style>
+    .tree, .tree ul {
+        margin:0;
+        padding:0;
+        list-style:none
+    }
+    .tree ul {
+        margin-left:1em;
+        position:relative
+    }
+    .tree ul ul {
+        margin-left:.5em
+    }
+    .tree ul:before {
+        content:"";
+        display:block;
+        width:0;
+        position:absolute;
+        top:0;
+        bottom:0;
+        left:0;
+        border-left:1px solid
+    }
+    .tree li {
+        margin:0;
+        padding:0 1em;
+        line-height:2em;
+        font-size: 17px;
+        color:#369;
+        font-weight:700;
+        position:relative
+    }
+    .tree ul li:before {
+        content:"";
+        display:block;
+        width:10px;
+        height:0;
+        border-top:1px solid;
+        margin-top:-1px;
+        position:absolute;
+        top:1em;
+        left:0
+    }
+    .tree ul li:last-child:before {
+        background:#fff;
+        height:auto;
+        top:1em;
+        bottom:0
+    }
+    .indicator {
+        margin-right:5px;
+    }
+    .tree li a {
+        text-decoration: none;
+        color:#369;
+    }
+    .tree li button, .tree li button:active, .tree li button:focus {
+        text-decoration: none;
+        color:#369;
+        border:none;
+        background:transparent;
+        margin:0px 0px 0px 0px;
+        padding:0px 0px 0px 0px;
+        outline: 0;
+    }
+</style>
+<script>
+    $.fn.extend({
+        treed: function (o) {
+
+            var openedClass = 'glyphicon-minus-sign';
+            var closedClass = 'glyphicon-plus-sign';
+
+            if (typeof o != 'undefined'){
+                if (typeof o.openedClass != 'undefined'){
+                    openedClass = o.openedClass;
+                }
+                if (typeof o.closedClass != 'undefined'){
+                    closedClass = o.closedClass;
+                }
+            };
+
+            //initialize each of the top levels
+            var tree = $(this);
+            tree.addClass("tree");
+            tree.find('li').has("ul").each(function () {
+                var branch = $(this); //li with children ul
+                branch.prepend("<i class='indicator glyphicon " + closedClass + "'></i>");
+                branch.addClass('branch');
+                branch.on('click', function (e) {
+                    if (this == e.target) {
+                        var icon = $(this).children('i:first');
+                        icon.toggleClass(openedClass + " " + closedClass);
+                        $(this).children().children().toggle();
+                    }
+                })
+                branch.children().children().toggle();
+            });
+            //fire event from the dynamically added icon
+            tree.find('.branch .indicator').each(function(){
+                $(this).on('click', function () {
+                    $(this).closest('li').click();
+                });
+            });
+            //fire event to open branch if the li contains an anchor instead of text
+            tree.find('.branch>a').each(function () {
+                $(this).on('click', function (e) {
+                    $(this).closest('li').click();
+                    e.preventDefault();
+                });
+            });
+            //fire event to open branch if the li contains a button instead of text
+            tree.find('.branch>button').each(function () {
+                $(this).on('click', function (e) {
+                    $(this).closest('li').click();
+                    e.preventDefault();
+                });
+            });
+        }
+    });
+
+    //Initialization of treeviews
+
+    $('#tree').treed();
+
+</script>
 @endsection
