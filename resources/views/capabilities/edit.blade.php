63cb2d82911eb57ef9427822d7956e37684cf103
emazeem07 <emazeem07@gmail.com>
2021-06-10 17:50:28 +0500

    ch

--- resources/views/capabilities/edit.blade.php
+++ resources/views/capabilities/edit.blade.php
@@ -1,113 +1,4 @@
-<script>
-    $(document).ready(function () {
-        $(".select-2-parameter").select2();
-        $(".select-2-procedure").select2();
-        $('#editparameter').on('change', function() {
-            var parameter = $(this).val();
-            if(parameter) {
-                $.ajax({
-                    url: '/units/fetch/previous_units/'+parameter,
-                    type: "GET",
-                    dataType: "json",
-                    success:function(data) {
-                        $('#editunit').empty();
-                        $('#editunit').append('<option disabled selected>--Select Units</option>');
-                        $.each(data['previous'], function(key, value) {
-                            $('#editunit').append('<option value="'+value.id+'">'+ value.unit +'</option>');
-                        });
-                    }
-                });
-            }
-            else{
-                $('#editunit').empty();
-                $('#editunit').append('<option disabled selected>--Select Unit</option>');
-            }
-        });
-        $(document).on('click', '.edit', function() {
-            var id = $(this).attr('data-id');
-            $.ajax({
-                "url": "{{url('/capabilities/edit')}}",
-                type: "POST",
-                data: {'id': id,_token: '{{csrf_token()}}'},
-                dataType : "json",
-                statusCode: {
-                    403: function() {
-                        swal("Failed", "Permission denied for this action." , "error");
-                        return false;
-                    }
-                },
-                success: function(data)
-                {
-                    $('#edit_capabilities').modal('toggle');
-                    $('#editid').val(data.id);
-                    $('#editname').val(data.name);
-                    $('#editparameter').val(data.parameter).trigger('change');
-                    $('#editprocedure').val(data.procedure).trigger('change');
-                    $('#edit_min_range').val(data.min_range);
-                    $('#edit_acc_min_range').val(data.accredited_min_range);
-                    $('#edit_max_range').val(data.max_range);
-                    $('#edit_acc_max_range').val(data.accredited_max_range);
 
-                    $('#editaccuracy').val(data.accuracy);
-                    $('#editcalculator').val(data.calculator);
-                    $('#editprice').val(data.price);
-                    $('#editremarks').val(data.remarks);
-                    $('#editlocation').val(data.location);
-                    if(data.accredited=='yes'){
-                        $("#editaccredited").prop('checked', true);
-                    }else {
-                        $("#editaccredited").prop('checked', false);
-                    }
-
-                    $('#editunit').empty();
-                    $('#editunit').append('<option disabled selected>--Select Units</option>');
-                    $.each(data['units'], function(key, value) {
-                        var selection = data.unit == value.id ? 'selected' : '';
-                        $('#editunit').append('<option value="'+value.id+'" '+selection+'>'+ value.unit +'</option>');
-                    });
-
-                    //Populating Form Data to Edit Ends
-                },
-                error: function(){},
-            });
-        });
-        $("#edit_capabilities_form").on('submit',(function(e) {
-            var button=$('.cap-update-btn');
-            var previous=$('.cap-update-btn').html();
-            button.attr('disabled','disabled').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Processing');
-
-            e.preventDefault();
-            $.ajax({
-                url: "{{route('capabilities.update')}}",
-                type: "POST",
-                data:  new FormData(this),
-                contentType: false,
-                cache: false,
-                processData:false,
-                success: function(data)
-                {
-                    button.attr('disabled',null).html(previous);
-                    swal('success',data.success,'success').then((value) => {
-                        $('#edit_capabilities').modal('hide');
-                        $("#example").DataTable().ajax.reload(null,false);
-                        //InitTable();
-                    });
-
-                },
-                error: function(xhr)
-                {
-                    button.attr('disabled',null).html(previous);
-                    var error='';
-                    $.each(xhr.responseJSON.errors, function (key, item) {
-                        error+=item;
-                    });
-                    swal("Failed", error, "error");
-                }
-            });
-        }));
-
-    });
-</script>
 <div class="modal fade" id="edit_capabilities" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -222,3 +113,113 @@
     </div>
 </div>
 </div>
+<script>
+    $(document).ready(function () {
+        $(".select-2-parameter").select2();
+        $(".select-2-procedure").select2();
+        $('#editparameter').on('change', function() {
+            var parameter = $(this).val();
+            if(parameter) {
+                $.ajax({
+                    url: '/units/fetch/previous_units/'+parameter,
+                    type: "GET",
+                    dataType: "json",
+                    success:function(data) {
+                        $('#editunit').empty();
+                        $('#editunit').append('<option disabled selected>--Select Units</option>');
+                        $.each(data['previous'], function(key, value) {
+                            $('#editunit').append('<option value="'+value.id+'">'+ value.unit +'</option>');
+                        });
+                    }
+                });
+            }
+            else{
+                $('#editunit').empty();
+                $('#editunit').append('<option disabled selected>--Select Unit</option>');
+            }
+        });
+        $(document).on('click', '.edit', function() {
+            var id = $(this).attr('data-id');
+            $.ajax({
+                "url": "{{url('/capabilities/edit')}}",
+                type: "POST",
+                data: {'id': id,_token: '{{csrf_token()}}'},
+                dataType : "json",
+                statusCode: {
+                    403: function() {
+                        swal("Failed", "Permission denied for this action." , "error");
+                        return false;
+                    }
+                },
+                success: function(data)
+                {
+                    $('#edit_capabilities').modal('toggle');
+                    $('#editid').val(data.id);
+                    $('#editname').val(data.name);
+                    $('#editparameter').val(data.parameter).trigger('change');
+                    $('#editprocedure').val(data.procedure).trigger('change');
+                    $('#edit_min_range').val(data.min_range);
+                    $('#edit_acc_min_range').val(data.accredited_min_range);
+                    $('#edit_max_range').val(data.max_range);
+                    $('#edit_acc_max_range').val(data.accredited_max_range);
+
+                    $('#editaccuracy').val(data.accuracy);
+                    $('#editcalculator').val(data.calculator);
+                    $('#editprice').val(data.price);
+                    $('#editremarks').val(data.remarks);
+                    $('#editlocation').val(data.location);
+                    if(data.accredited=='yes'){
+                        $("#editaccredited").prop('checked', true);
+                    }else {
+                        $("#editaccredited").prop('checked', false);
+                    }
+
+                    $('#editunit').empty();
+                    $('#editunit').append('<option disabled selected>--Select Units</option>');
+                    $.each(data['units'], function(key, value) {
+                        var selection = data.unit == value.id ? 'selected' : '';
+                        $('#editunit').append('<option value="'+value.id+'" '+selection+'>'+ value.unit +'</option>');
+                    });
+
+                    //Populating Form Data to Edit Ends
+                },
+                error: function(){},
+            });
+        });
+        $("#edit_capabilities_form").on('submit',(function(e) {
+            var button=$('.cap-update-btn');
+            var previous=$('.cap-update-btn').html();
+            button.attr('disabled','disabled').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Processing');
+
+            e.preventDefault();
+            $.ajax({
+                url: "{{route('capabilities.update')}}",
+                type: "POST",
+                data:  new FormData(this),
+                contentType: false,
+                cache: false,
+                processData:false,
+                success: function(data)
+                {
+                    button.attr('disabled',null).html(previous);
+                    swal('success',data.success,'success').then((value) => {
+                        $('#edit_capabilities').modal('hide');
+                        $("#example").DataTable().ajax.reload(null,false);
+                        //InitTable();
+                    });
+
+                },
+                error: function(xhr)
+                {
+                    button.attr('disabled',null).html(previous);
+                    var error='';
+                    $.each(xhr.responseJSON.errors, function (key, item) {
+                        error+=item;
+                    });
+                    swal("Failed", error, "error");
+                }
+            });
+        }));
+
+    });
+</script>
