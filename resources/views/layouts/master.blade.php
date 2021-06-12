40fdb5d17d10054a813c48bf3f0db9e5540d4de2
emazeem07 <emazeem07@gmail.com>
2021-06-12 14:37:32 +0500

    ch

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
 
     <link rel="icon" href="{{url('img/rubic-logo-favicon.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
-    <link rel="stylesheet" href="{{url('css/style.css')}}">
+    <link rel="stylesheet" href="{{url('css/styling.css')}}">
     <script type="text/javascript" src="{{url('js/sweetalert.min.js')}}"></script>
     <link rel="stylesheet" href="{{url('assets/css/plugins/select2.min.css')}}">
 </head>
