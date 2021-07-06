f553d3c8003f739fc867d69de0407460e79cf19a
emazeem07 <emazeem07@gmail.com>
2021-07-06 11:25:55 +0500

    f

--- resources/views/paymentvoucher/create.blade.php
+++ resources/views/paymentvoucher/create.blade.php
@@ -222,7 +222,7 @@
                 count++;
                 var newRow = $("<tr>");
                 var cols = "";
-                cols += '<td><select name="tlevel[]"  class="form-control tlevel" id="tlevel'+count+'" data-tlevel="'+count+'"><option value="" selected>Select Account</option>@php foreach ($accounts as $account){ echo '<option value="'.$account->id.'">'.$account->title.'</option>';}  @endphp</td>';
+                cols += '<td><select name="tlevel[]"  class="form-control tlevel" id="tlevel'+count+'" data-tlevel="'+count+'"><option value="" selected>Select Account</option>@php foreach ($accounts as $account){ echo '<option value="'.$account->id.'">'.$account->codethree->title.'-'.$account->title.'</option>';}  @endphp</td>';
                 cols += '<td><select name="account[]"  class="form-control account" id="account_id'+count+'" data-account_id="'+count+'"><option value="" selected>Select Account</option>';
                 cols += '<td><select name="costcenter[]"  class="form-control costcenter_id" id="costcenter_id'+count+'"><option value="" selected>Select Cost Center</option></select></td>';
                 cols += '<td><textarea rows="1"  class="form-control" name="narration[]" placeholder="Narration"/></td>';
