2a8847c7136f9713fb484ad9dc873ca7831462ff
emazeem07 <emazeem07@gmail.com>
2021-07-07 12:49:58 +0500

    f

--- resources/views/delivery_note/create.blade.php
+++ resources/views/delivery_note/create.blade.php
@@ -5,45 +5,46 @@
     }
 </style>
 <div class="modal fade" id="add_delivery_note" tabindex="-1" role="dialog" aria-labelledby="titleDeliveryNote" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-body">
-
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 border-bottom mb-3 pb-2">
                         <h5 class="modal-title font-weight-light float-left" id="titleDeliveryNote"><i class="feather icon-plus-circle"></i> Add Delivery Note</h5>
                         <button type="button" class="close close-btn float-right" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true"><i class="feather icon-x-circle"></i></span>
                         </button>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <form id="add_delivery_note_form">
-                            @csrf
 
+                    <form id="add_delivery_note_form">
+                        @csrf
+                        <input type="hidden" name="job_id" value="{{$job->id}}">
+                        <input type="hidden" name="delivery_item_id" id="delivery_item_id"  value="">
+                        {{--<div class="row">
+                            <div class='checkbox text-right checkbox-fill checkbox-danger d-inline'>
+                                <input type='checkbox' class="text-right" id='check-all'>
+                                <label class='cr text-right font-weight-bold text-danger' for='check-all' style="text-decoration: underline">Mark All</label>
+                            </div>
 
-                            <input type="hidden" name="job_id" value="{{$job->id}}">
-                            <input type="hidden" name="delivery_item_id" id="delivery_item_id"  value="">
-                            <div class="row">
+                        </div>
+                        --}}
+                        @if(count($job->jobitems)>0)
+                            <div class="col-12 p-0 m-0">
                                 <div class='checkbox text-right checkbox-fill checkbox-danger d-inline'>
                                     <input type='checkbox' class="text-right" id='check-all'>
                                     <label class='cr text-right font-weight-bold text-danger' for='check-all' style="text-decoration: underline">Mark All</label>
                                 </div>
                             </div>
-                            <div class="row">
-
-                                @foreach($job->jobitems as $item)
-                                    <div class='checkbox col-6 checkbox-fill d-inline'>
-                                        <input type='checkbox' data-id='{{$item->id}}' name='delivery_items' class='delivery_items' id='delivery_items{{$item->id}}'
+                        @endif
+                        @foreach($job->jobitems as $item)
+                            <div class='checkbox col-md-4 checkbox-fill d-inline'>
+                                <input type='checkbox' data-id='{{$item->id}}' name='delivery_items' class='delivery_items' id='delivery_items{{$item->id}}'
                                         {{(in_array($item->id,$delivered_id)?'selected disabled':'')}}>
-                                        <label class='cr {{(in_array($item->id,$delivered_id)?'line-through':'')}}' for='delivery_items{{$item->id}}' >{{$item->item->capabilities->name}}</label>
-                                    </div>
-                                @endforeach
+                                <label class='cr {{(in_array($item->id,$delivered_id)?'line-through':'')}}' for='delivery_items{{$item->id}}' >{{$item->item->capabilities->name}}</label>
                             </div>
-                            <button class="btn btn-primary btn-sm rounded float-right delivery-note-add-btn" type="submit"><i class="feather icon-save"></i> Save</button>
-                        </form>
-                    </div>
+                        @endforeach
+                        <button class="btn btn-primary btn-sm rounded float-right delivery-note-add-btn" type="submit"><i class="feather icon-save"></i> Save</button>
+                    </form>
                 </div>
             </div>
         </div>
